18d11699f9d29ae096dcb32604b98fbdcc9bcdab
ilhan-moha <mohamudilhan99@gmail.com>
2026-06-11 10:07:50 +0300

    Fix Book and Coffee classes

--- lib/coffee.py
+++ lib/coffee.py
@@ -4,19 +4,27 @@ class Coffee:
     def __init__(self, size, price):
         self.size = size
         self.price = price
-    
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+            self._size = None
+
     @property
     def price(self):
-            return self._price
-    
+        return self._price
+
     @price.setter
     def price(self, value):
-            if value in ["small", "medium", "large"]:
-                self._price = value
-            else:
-                    print("size must be 'small', 'medium', or 'large'")
-                    self._price = None
-    
+        self._price = value
+
     def tip(self):
-        print("This coffee is great, here's a tip!")
+        print("shoe has been repaired")
         self.price += 1
