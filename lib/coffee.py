ead8a6576ee3c9525e04fc2271f2e0c00062c979
ilhan-moha <mohamudilhan99@gmail.com>
2026-06-11 09:32:59 +0300

    Added Book and Coffee classes with validation

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+    
+    @property
+    def price(self):
+            return self._price
+    
+    @price.setter
+    def price(self, value):
+            if value in ["small", "medium", "large"]:
+                self._price = value
+            else:
+                    print("size must be 'small', 'medium', or 'large'")
+                    self._price = None
+    
+    def tip(self):
+        print("This coffee is great, here's a tip!")
+        self.price += 1
